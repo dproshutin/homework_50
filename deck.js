8037394cb2da9690bb1e759fbbbf93d596c98923
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-09-22 18:52:35 +0600

    The class Deck is implemented.

---
+++ deck.js
@@ -0,0 +1,40 @@
+class Deck {
+    constructor() {
+        const RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        const SUITS = ["\u2660", "\u2665", "\u2666", "\u2663"];
+        this.deck = [];
+        for (let i = 0; i < RANKS.length; i++) {
+            for (let j = 0; j < SUITS.length; j++) {
+                let card = {};
+                card.rank = RANKS[i];
+                card.suit = SUITS[j];
+                this.deck.push(card);
+            }
+        }
+    }
+    _getRandomIndexFromArray(arr) {
+        let max = arr.length;
+        let min = 0;
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    _getRandomCardFromDeck(deck) {
+        let index = this._getRandomIndexFromArray(deck);
+        return deck.splice(index, 1)[0];
+    }
+    shuffle() {
+        let newDeck = [];
+        while (this.deck.length > 0) {
+            let randomCard = this._getRandomCardFromDeck(this.deck);
+            newDeck.push(randomCard);
+        }
+        this.deck = [...newDeck];
+        console.log(newDeck);
+    }
+    drawCardFromDeckOnTop() {
+        return this.deck.shift();
+    }
+}
+
+let myDeck = new Deck();
+myDeck.shuffle();
+console.log(myDeck.drawCardFromDeckOnTop());
